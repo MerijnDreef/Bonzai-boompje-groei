5df92eeff07d515e181e26245c8d57736e9baa36
Merijn <99054535@mydavinci.nl>
2022-09-07 15:22:28 +0200

    failed to add up in javascript

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var leafsAmount = document.getElementById("leafs");
 var daysAmount = document.getElementById("days");
 var calculate = document.getElementById("calculate")
+calculate.setAttribute('onclick', 'calcDays()');
+// calculate.onclick = function(){calcDays(leafsAmount, daysAmount);};
+var initialAmount = leafsAmount.value;
 
-calculate.onclick = function(){calcDays(leafsAmount, daysAmount);};
-
-function calcDays(leafsCount, daysCount) {
-   var bonzaiSize = leafsCount + (leafsCount / 100 * 4);
+function calcDays() {
+   console.log(initialAmount)
+   var addUp = leafsAmount.value/100*4;
+   var bonzaiSize = initialAmount+=addUp;
+   // daysAmount = 0;
    console.log(bonzaiSize);
 }
