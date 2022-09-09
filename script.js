533a2e4a0b62482d20027dc5d0d4c051c504aeb7
Merijn <99054535@mydavinci.nl>
2022-09-09 10:09:29 +0200

    added a p, so it will show on screen the growth and the decimal problem is solved
    added a math.ceil so it rounds the decimal up all the time, and added a p so it will visually show it

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var leafsAmount = document.getElementById("leafs");
 var daysAmount = document.getElementById("days");
-var calculate = document.getElementById("calculate")
+var calculate = document.getElementById("calculate");
+var growthTotal = document.getElementById("growth");
+var cutTotal = document.getElementById("cut");
+
 calculate.setAttribute('onclick', 'calcDays()');
 // calculate.onclick = function(){calcDays(leafsAmount, daysAmount);};
 
+
 function calcDays() {
    var daysCount = daysAmount.value * 1;
+   var cutCount = 0;
    console.log(daysCount);
    var initialAmount = 1 * leafsAmount.value;
    var addUp = leafsAmount.value / 100 * 4;
@@ -13,22 +18,29 @@ function calcDays() {
    // console.log(addUp);
    var bonzaiSize = initialAmount += addUp;
    console.log("pre vibe check");
-   for (var i = 0; i < daysCount; i++) {
+   for (var i = 1; i < daysCount; i++) {
       console.log("post vibe check");
       console.log(initialAmount);
       if (addUp >= 200) {
+
          addUp = addUp / 100 * 8;
+
+         // cutCount = 
          console.log("You have failed the vibe check");
       }
       console.log(addUp);
-      Math.round(addUp);
+      addUp = Math.ceil(addUp);
+      console.log(addUp);
       bonzaiSize = initialAmount += addUp;
       initialAmount = bonzaiSize;
       addUp = initialAmount / 100 * 4;
       console.log(initialAmount);
       console.log(addUp);
+      console.log(Counter);
    }
    // daysAmount = 0;
-   console.log("the end")
+   console.log("the end");
+   growthTotal.innerText = bonzaiSize;
+   // growthTotal.appendChild(growthTotal);
    console.log(bonzaiSize);
 }
